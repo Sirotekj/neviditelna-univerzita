f0e60172a5bdde3b1a7393d0fb1fdd61242aa062
Sirotekj <sirotek.j@gmail.com>
2026-02-17 16:17:25 +0100

    Header - name and Icon

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,21 +1,49 @@
 <template>
   <header class="header">
-    <IconMenu />
-    <h1>Ferda</h1>
+    <div class="left">
+      <IconMenu />
+      <h1>Ferda</h1>
+    </div>
+    <div class="right">
+      <button class="icon-button">
+        <IconAccountCircle size="24" />
+      </button>
+      <label>Jan Musílek</label>
+    </div>
   </header>
 </template>
 <script setup lang="ts">
 import IconMenu from '../icons/IconMenu.vue'
+import IconAccountCircle from '../icons/IconAccountCircle.vue'
 </script>
-<style>
+<style scoped>
 .header {
   height: 70px;
   display: flex;
   align-items: center;
-  gap: 24px;
+  justify-content: space-between;
   padding: 0 24px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
   color: white;
   background: #2196f3;
+}
+.header div {
+  display: flex;
+  align-items: center;
+}
+.left {
+  gap: 24px;
+}
+.right {
+  gap: 12px;
+  text-transform: uppercase;
+}
+.icon-button {
+  padding: 4px;
+  all: unset;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 }
 </style>
